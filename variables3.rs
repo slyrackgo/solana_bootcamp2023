92db06c514e31487fd308d249ff0fe304a89b7ce
Atai <58735260+slyrackgo@users.noreply.github.com>
2023-05-29 16:42:57 +0200

    Delete variables3.rs

--- variables3.rs
+++
@@ -1,9 +0,0 @@
-// variables3.rs
-// Make me compile! Execute the command `rustlings hint variables3` if you want a hint :)
-
-// I AM NOT DONE
-
-fn main() {
-    let x: i32;
-    println!("Number {}", x);
-}
